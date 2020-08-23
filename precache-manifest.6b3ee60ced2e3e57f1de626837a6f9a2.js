4e929c36e1acb3261e97ae5349b98bbe322e970b
Sam Halfpenny <sam.j.halfpenny@gmail.com>
2020-08-24 01:18:13 +0200

    Updates

---
+++ precache-manifest.6b3ee60ced2e3e57f1de626837a6f9a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c474030b32a7e483fa970131b8cbec7",
+    "url": "/facebrain/index.html"
+  },
+  {
+    "revision": "a8fba5bf931e5808b39c",
+    "url": "/facebrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "852e8459c9a3c5518c48",
+    "url": "/facebrain/static/css/main.4dbb2c21.chunk.css"
+  },
+  {
+    "revision": "a8fba5bf931e5808b39c",
+    "url": "/facebrain/static/js/2.e603e980.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facebrain/static/js/2.e603e980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "852e8459c9a3c5518c48",
+    "url": "/facebrain/static/js/main.1f5adae3.chunk.js"
+  },
+  {
+    "revision": "57e8724984bb350e7691",
+    "url": "/facebrain/static/js/runtime-main.2b47dffe.js"
+  }
+]);
